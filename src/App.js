58c73f9cad55d3b48e7b9ed5b9ea8c9c4f8dbe9b
JoelNickson <joel2000nickson@gmail.com>
2022-02-02 04:50:37 +0530

    init react-tailwind app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './styles/App.css';
+import About from './components/about';
+
+function App() {
+  return (
+    <section className='mx-10  text-white'>
+      <About />
+    </section>
+
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgblue: "#3f8dac"
+      }
+    },
+  },
+  plugins: [],
+}
