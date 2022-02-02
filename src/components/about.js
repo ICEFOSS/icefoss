a7ec3cdbf94cb847991c014fd2793b73641556e9
JoelNickson <joel2000nickson@gmail.com>
2022-02-02 13:16:34 +0530

    added ffsc img to about

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,8 @@ export default class About extends Component {
         return <>
             <h2 className='text-5xl my-10 ml-10'>About</h2>
             <AboutEachSection
-                imgSrc='/assets/fisat.webp'
+                imgSrc='/assets/ffsc.png'
+                padding='20'
                 title="FFSC"
                 desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta eius omnis libero excepturi odit pariatur, unde, expedita repudiandae dolorem sunt, harum architecto modi! Nam exercitationem aliquid architecto corrupti, iure vitae!"
             />
@@ -22,7 +23,9 @@ export function AboutEachSection(props) {
     return (
         <div className='bg-opacity-20 bg-gray-300 p-10 rounded-md m-6'>
             <div className='text-black flex flex-wrap flex-row justify-center'>
-                <img className='aspect-auto lg:w-5/12 my-3 lg:mr-9 rounded-lg' src={props.imgSrc} alt='' />
+                <div className={`lg:w-5/12 my-3 lg:mr-9 p-${props.padding} bg-white bg-opacity-20 rounded-lg`}>
+                    <img className='aspect-auto' src={props.imgSrc} alt='' />
+                </div>
                 <div className='lg:w-6/12 items-center text-white lg:mx-3 flex flex-col justify-center'>
                     <h3 className='text-4xl'>{props.title}</h3>
                     <p className='mt-3 text-center rounded-md'>
