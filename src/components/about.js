ad9e8d6e13a422c7e2e105f451393fff1cf1c570
JoelNickson <joel2000nickson@gmail.com>
2022-02-02 15:24:43 +0530

    about image made smaller on md screens

--- src/components/about.js
+++ src/components/about.js
@@ -5,14 +5,14 @@ export default class About extends Component {
         return <>
             <h2 className='text-5xl my-10 ml-10'>About</h2>
             <AboutEachSection
-                imgSrc='/assets/ffsc.png'
-                padding='20'
-                title="FFSC"
+                imgSrc='/assets/fisat.webp'
+                title="FISAT"
                 desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta eius omnis libero excepturi odit pariatur, unde, expedita repudiandae dolorem sunt, harum architecto modi! Nam exercitationem aliquid architecto corrupti, iure vitae!"
             />
             <AboutEachSection
-                imgSrc='/assets/fisat.webp'
-                title="FISAT"
+                imgSrc='/assets/ffsc.png'
+                p='px-16 py-6 md:px-52 md:py-12 lg:p-20'
+                title="FFSC"
                 desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta eius omnis libero excepturi odit pariatur, unde, expedita repudiandae dolorem sunt, harum architecto modi! Nam exercitationem aliquid architecto corrupti, iure vitae!"
             />
         </>;
@@ -20,15 +20,16 @@ export default class About extends Component {
 }
 
 export function AboutEachSection(props) {
+    console.log(props);
     return (
-        <div className='bg-opacity-20 bg-gray-300 p-10 rounded-md m-6'>
-            <div className='text-black flex flex-wrap flex-row justify-center'>
-                <div className={`lg:w-5/12 my-3 lg:mr-9 p-${props.padding} bg-white bg-opacity-20 rounded-lg`}>
-                    <img className='aspect-auto rounded-lg' src={props.imgSrc} alt='' />
+        <div className='bg-opacity-20 bg-gray-300 p-3 lg:p-10 rounded-md my-6 lg:m-6'>
+            <div className='text-black flex flex-wrap flex-col md:flex-row justify-center'>
+                <div className={`lg:w-5/12 w-fit bg-white bg-opacity-20 rounded-lg`}>
+                    <img className={`aspect-auto rounded-lg ${props.p}`} src={props.imgSrc} alt='photo' />
                 </div>
                 <div className='lg:w-6/12 items-center text-white lg:mx-3 flex flex-col justify-center'>
                     <h3 className='text-4xl'>{props.title}</h3>
-                    <p className='mt-3 text-center rounded-md'>
+                    <p className='mt-3 px-2 lg:text-center rounded-md'>
                         {props.desc}
                     </p>
                 </div>
