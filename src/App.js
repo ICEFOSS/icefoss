ab7a0efd85de897b681186e4e0b9d5a3da49a5e0
AparnaJoshi555 <aparnajoshi789@gmail.com>
2022-02-03 12:26:49 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './styles/App.css';
 import About from './components/about';
 import Speaker from './components/speakers';
-
+import Footer from './components/footer';
 function App() {
   return (
-    <section className='mx-10  text-white'>
+   <>
+   
+   <section className='mx-10  text-white'>
+      
       <About />
       <Speaker />
+
+     
     </section>
+    <section>
+       <Footer/>
+    </section>
+    
+    </>
 
   );
 }
