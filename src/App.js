522085bf7755cb1695434354b29ddf9972c258a6
ashmisubramanian <ashmisubramanian0512@gmail.com>
2022-02-03 12:09:56 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './styles/App.css';
 import About from './components/about';
 import Speaker from './components/speakers';
 
+import Workshop from './components/workshops';
+
 function App() {
   return (
     <section className='mx-10  text-white'>
       <About />
       <Speaker />
+      <Workshop />
     </section>
 
   );
