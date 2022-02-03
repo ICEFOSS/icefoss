b03a9d10bf9fc516af5912900ae84c57a92b3b69
Amar1013 <amarjithraj67@gmail.com>
2022-02-03 12:36:42 +0530

    navbar and landing page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './styles/App.css';
 import About from './components/about';
 import Speaker from './components/speakers';
+import Landing from './components/landing';
+import Nav from './components/nav';
 
 function App() {
   return (
-    <section className='mx-10  text-white'>
-      <About />
-      <Speaker />
+    <><section>
+      <Nav />
     </section>
+    <section className='mx-10  text-gray-800'>
+        <Landing />
+     </section>
+     <section className='mx-10 md:mt-10 mt-28 text-gray-800'>   
+        <About />
+        <Speaker />
+      </section></>
 
   );
 }
