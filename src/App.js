a5fb8ac6f90905137e17165cb1c79c4bdb899607
Amar1013 <amarjithraj67@gmail.com>
2022-02-04 09:37:29 +0530

    waves added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import About from './components/about';
 import Speaker from './components/speakers';
 import Landing from './components/landing';
 import Nav from './components/nav';
-
 import Workshop from './components/workshops';
+import {Bubble_svg} from './components/constants'
 
 function App() {
   return (
@@ -14,9 +14,24 @@ function App() {
     <section className='mx-10  text-gray-800'>
         <Landing />
      </section>
-     <section className='mx-10 md:mt-10 mt-28 text-gray-800'>   
+     <div className='flex justify-center w-full overflow-hidden'>
+          
+          <Bubble_svg />
+        </div>
+      
+     <section className='mx-10 md:mt-10 mt-28 text-gray-800'> 
         <About />
+      </section>
+      <div className='flex justify-center w-full mx-0'>
+          <Bubble_svg />
+        </div>
+        <section className='mx-10 md:mt-10 mt-28 text-gray-800'>
         <Speaker />
+        </section>
+        <div className='flex justify-center w-full'>
+          <Bubble_svg />
+        </div>
+        <section className='mx-10 md:mt-10 mt-28 text-gray-800'>
         <Workshop />
       </section></>
 
