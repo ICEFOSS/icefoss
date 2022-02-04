9b815d393877c574795a39e9d060cdc71fc057c1
JoelNickson <joel2000nickson@gmail.com>
2022-02-04 19:43:31 +0530

    removed report section from nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -43,13 +43,6 @@ function Nav() {
                   >
                     Workshops
                   </a>
-
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Reports
-                  </a>
                 </div>
               </div>
             </div>
@@ -140,12 +133,6 @@ function Nav() {
                   Workshops
                 </a>
 
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Reports
-                </a>
               </div>
             </div>
           )}
