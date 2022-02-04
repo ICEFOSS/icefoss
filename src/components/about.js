c46a9ea7840678f964e3f7fb16ce288e21b78b61
JoelNickson <joel2000nickson@gmail.com>
2022-02-05 03:56:18 +0530

    changed layout in large screen

--- src/components/about.js
+++ src/components/about.js
@@ -25,13 +25,14 @@ export default class About extends Component {
 
 export function AboutEachSection(props) {
     return (
-        <div className='bg-opacity-50 bg-white shadow-sm shadow-black p-3 lg:p-10 rounded-md my-6 lg:m-6'>
-            <div className='text-black flex flex-wrap flex-col md:flex-row justify-center'>
-                <div className={`lg:w-5/12 mt-4 mb-10 w-fit from-blue-400 to-sky-300 bg-gradient-to-br bg-opacity-10 shadow-md shadow-black rounded-lg saturate-150 customname`}>
-                    <img className={`aspect-auto rounded-lg ${props.p} shadow-slate-600 shadow-lg h-full`} src={props.imgSrc} alt='photo' />
+        <div className='bg-opacity-50 bg-white shadow-sm shadow-black p-3 lg:p-10 rounded-md mt-6 lg:m-6'>
+            <div className='text-black flex flex-wrap flex-col md:flex-row justify-around w-full'>
+                <h3 className='text-4xl text-center lg:hidden block'>{props.title}</h3>
+                <div className={`xl:w-6/12 mt-4 mb-10 w-fit from-blue-400 to-sky-300 bg-gradient-to-br bg-opacity-10 shadow-sm shadow-blue-300 rounded-lg saturate-150 customname mx-6`}>
+                    <img className={`aspect-auto rounded-lg ${props.p} shadow-slate-600 shadow-sm h-full`} src={props.imgSrc} alt='photo' />
                 </div>
-                <div className='lg:w-6/12 items-center text-black lg:mx-3 flex flex-col justify-center'>
-                    <h3 className='text-4xl'>{props.title}</h3>
+                <div className='xl:w-5/12 items-center text-black lg:mx-3 flex flex-col justify-center'>
+                    <h3 className='text-4xl hidden lg:block'>{props.title}</h3>
                     <p className='mt-3 px-2 lg:text-center rounded-md bg-blend-saturation'>
                         {props.desc}
                     </p>
