e9530ae09e31e0ed7e7c613bd255ed80eabb6309
JoelNickson <joel2000nickson@gmail.com>
2022-02-05 02:47:08 +0530

    add: top&bottom padding to about

--- src/components/about.js
+++ src/components/about.js
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 
 export default class About extends Component {
     render() {
-        return <>
-            <h2 className='text-5xl my-10 ml-10' id='About'>About</h2>
-            <AboutEachSection
-                imgSrc='/assets/fisat.webp'
-                title="FISAT"
-                desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta eius omnis libero excepturi odit pariatur, unde, expedita repudiandae dolorem sunt, harum architecto modi! Nam exercitationem aliquid architecto corrupti, iure vitae!"
-            />
-            <AboutEachSection
-                imgSrc='/assets/ffsc.png'
-                p='px-16 py-6 md:px-52 md:py-12 lg:px-20'
-                title="FFSC"
-                desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta eius omnis libero excepturi odit pariatur, unde, expedita repudiandae dolorem sunt, harum architecto modi! Nam exercitationem aliquid architecto corrupti, iure vitae!"
-            />
-        </>;
+        return (
+            <div className="py-40">
+                <h2 className='text-5xl text-center' id='About'>About</h2>
+                <AboutEachSection
+                    imgSrc='/assets/fisat.webp'
+                    title="FISAT"
+                    desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta eius omnis libero excepturi odit pariatur, unde, expedita repudiandae dolorem sunt, harum architecto modi! Nam exercitationem aliquid architecto corrupti, iure vitae!"
+                />
+                <AboutEachSection
+                    imgSrc='/assets/ffsc.png'
+                    p='px-16 py-6 md:px-52 md:py-12 lg:px-20'
+                    title="FFSC"
+                    desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta eius omnis libero excepturi odit pariatur, unde, expedita repudiandae dolorem sunt, harum architecto modi! Nam exercitationem aliquid architecto corrupti, iure vitae!"
+                />
+                <br />
+                <br />
+            </div>
+        )
     }
 }
 
@@ -24,7 +28,7 @@ export function AboutEachSection(props) {
         <div className='bg-opacity-50 bg-white shadow-sm shadow-black p-3 lg:p-10 rounded-md my-6 lg:m-6'>
             <div className='text-black flex flex-wrap flex-col md:flex-row justify-center'>
                 <div className={`lg:w-5/12 mt-4 mb-10 w-fit from-blue-400 to-sky-300 bg-gradient-to-br bg-opacity-10 shadow-md shadow-black rounded-lg saturate-150 customname`}>
-                    <img className={`aspect-auto rounded-lg ${props.p} shadow-slate-600 shadow-lg`} src={props.imgSrc} alt='photo' />
+                    <img className={`aspect-auto rounded-lg ${props.p} shadow-slate-600 shadow-lg h-full`} src={props.imgSrc} alt='photo' />
                 </div>
                 <div className='lg:w-6/12 items-center text-black lg:mx-3 flex flex-col justify-center'>
                     <h3 className='text-4xl'>{props.title}</h3>
