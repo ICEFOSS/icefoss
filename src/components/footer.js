0735b79dab11f6a979e6dc3eadd2e5e4ae2a580f
JoelNickson <joel2000nickson@gmail.com>
2022-02-06 14:47:15 +0530

    dark(invertedd color) google map

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,8 +7,8 @@ export default function Footer() {
                     <div className="container h-[35rem] md:h-[32rem] pb-6 mx-auto flex flex-nowrap flex-col md:flex-row">
 
                         <ContactSec />
-                        <div className="w-full mt-8  opacity-30 hover:opacity-50 transition-opacity duration-500 rounded-lg overflow-hidden p-1 flex relative from-blue-300 to-green-500 bg-gradient-to-br  ">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3926.3671449790004!2d76.40663386471358!3d10.23194389269231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b08068aa17bd247%3A0xf048b9ebcbd2af28!2sFederal%20Institute%20of%20Science%20And%20Technology%20(FISAT)%C2%AE!5e0!3m2!1sen!2sin!4v1643976276450!5m2!1sen!2sin" width="100%" height="100%" frameBorder="0" loading="lazy"></iframe>
+                        <div className="w-full mt-8  opacity-70 hover:opacity-100 transition-opacity duration-500 rounded-lg overflow-hidden p-1 flex relative from-[#241414] to-[#01203C] bg-gradient-to-br  ">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3926.3671449790004!2d76.40663386471358!3d10.23194389269231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b08068aa17bd247%3A0xf048b9ebcbd2af28!2sFederal%20Institute%20of%20Science%20And%20Technology%20(FISAT)%C2%AE!5e0!3m2!1sen!2sin!4v1643976276450!5m2!1sen!2sin" width="100%" height="100%" frameBorder="0" loading="lazy" style={{ filter: "invert(90%)" }}></iframe>
 
                         </div>
                     </div>
