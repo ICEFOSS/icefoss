4a57bf9910759769021cd652d1651b1d271b791d
JoelNickson <joel2000nickson@gmail.com>
2022-02-14 01:07:07 +0530

    about.js:update shadow and border

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ export default class About extends Component {
             // <div className="py-40">
             //     <h2 className='text-5xl text-center' id='About'>About</h2>
             <section className="text-gray-200">
-                <div className="container sm:px-5 py-24 mx-auto">
+                <div className="sm:px-5 py-24 w-full">
                     <h2 className='text-5xl text-center mb-10' id='About'>About</h2>
                     {/* <div className="flex flex-wrap -mx-4 -mb-10 text-center"> */}
                     <div className=" flex flex-wrap flex-row justify-center gap-8">
@@ -37,8 +37,8 @@ export function AboutEachSection(props) {
     const aboutStyle = `w-fit rounded-md px-3 text-sm font-semibold text-gray-700 mr-2 mb-2 bg-opacity-0 ${props.hide}`
     return (
 
-        <div className="max-w-md overflow-hidden  flex flex-col justify-start rounded-2xl shadow-lg shadow-black group">
-            <div className='group-hover:-translate-y-1 transform bg-gray-700 bg-opacity-0 group-hover:bg-opacity-5 shadow-sm group-hover:shadow-lg group-hover:shadow-black shadow-black  rounded-2xl transition-all duration-200 w-[99%] mx-auto mt-1'>
+        <div className="max-w-xs overflow-hidden  flex flex-col justify-start rounded-lg shadow-xl hover:shadow-xl group">
+            <div className='group-hover:-translate-y-1 transform bg-gray-700 bg-opacity-0 group-hover:bg-opacity-5 shadow-sm group-hover:shadow-lg group-hover:shadow-sky-700 shadow-sky-900  rounded-lg transition-all duration-100 w-full mx-auto border-[1px] border-sky-600'>
                 <img className="w-full" src={props.imgSrc} alt="Forest" />
             </div>
             <div className="px-6 py-4">
