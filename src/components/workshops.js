66f6a4150c54b9cbf89bf46294e65af148207ae5
JoelNickson <joel2000nickson@gmail.com>
2022-02-14 01:10:22 +0530

    workshops.js:add rough content and custom scroll-bar on large devices

--- src/components/workshops.js
+++ src/components/workshops.js
@@ -6,17 +6,32 @@ export default class Workshop extends Component {
       <div className=" py-40" id="Workshops">
         <h2 className="text-5xl mb-20 text-sky-200 flex justify-center">Workshops</h2>
         <div className="flex flex-row flex-wrap justify-center gap-3">
-          <ProblemEachSection
+          <WorkshopEachSection
             id='0'
-            title="Title"
-            description={`Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto quis sit velit. Atque, ex! Maiores tempore quaerat repudiandae beatae quia veniam neque illo, enim nulla quisquam sint corrupti officia error?`}
+            title="Robot Operating System (ROS)"
+            description={`Day 1 : Will have a 🔥 chat session with resources, collecting the ideas and potential, which they have.
+            Get some feedback about their expectation about the intership.
+            Starts of by making a motivation to the domain from basics to a professional exposure.
+            Will discuss about certifications which firm's offers and that can create an impact in job search.
+            Start of with basic Kali Linux tools.
+            
+            Day 2 : Explore more Kali Linux tools, HTB, Tryhackme and online tools. Get an idea about CTF challenges and the relevance of that. Perform a system/android level hacking.
+            
+            Day 3: Introduction to VAPT, how to perform web hacking(XSS, CSRF, IDOR...etc).How to submit bug bounty reports etc. Discussions about pentesterlabs and other learning resources.
+            
+            Day 4: Introduction to SOC, how to fetch log from servers, how to analyse them. Deep dive into OSINT Tools.
+            
+            Day 5: Completing the SPLUNK fundamentals. Deepdive  into HTB, TRYHACKME, PENTESTERLABS.
+            
+            Day 6: Project discussions, how to get a job in this field. How to sustain in this field with just bug bounty. Linkedin in exploration for best learning insights. Free learning materials and paid learning materials discussions.
+            `}
             link="#"
           // function={this.scrollToAbout}
           />
 
-          <ProblemEachSection
+          <WorkshopEachSection
             id='1'
-            title="Title"
+            title="Cyber Security and Ethical Hacking"
             description={`
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto quis sit velit. Atque, ex! Maiores tempore quaerat repudiandae beatae quia veniam neque illo, enim nulla quisquam sint corrupti officia error?`}
             link="#"
@@ -28,16 +43,16 @@ export default class Workshop extends Component {
   }
 }
 
-function ProblemEachSection(props) {
+function WorkshopEachSection(props) {
 
   return (
     <>
       <div className="my-1 px-1 md:p-2 w-full sm:w-10/12 md:w-7/12 lg:w-5/12">
-        <a className="relative block group h-[22rem] active:h-[22rem] hover:h-[22rem] focus:h-[22rem] md:h-[19rem] md:hover:h-[19rem]">
+        <div className="relative block group h-[22rem] active:h-[22rem] hover:h-[22rem] focus:h-[22rem] md:h-[19rem] md:hover:h-[19rem]">
 
-          <div className="relative flex items-end h-full transform bg-black bg-opacity-10 shadow-md shadow-black  rounded-xl transition-all duration-200">
+          <div className="relative flex items-end h-full transform overflow-hidden group-hover:overflow-y-scroll bg-black bg-opacity-10 shadow-md shadow-black  rounded-xl transition-all duration-200">
 
-            <div className="px-8 pb-8 transition-opacity group-hover:opacity-0 group-hover:absolute">
+            <div className="px-8 pb-8 transition-opacity hover:opacity-0 group-hover:hidden">
 
               {(props.top == 'true') ? (
                 <h2 className="mb-10 text-xl">
@@ -69,8 +84,8 @@ function ProblemEachSection(props) {
               </button>
             </div>
           </div>
-        </a>
-      </div>
+        </div >
+      </div >
     </>
   )
 }
